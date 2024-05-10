f91a1784a0cc3773854cbed10ad7643a2ac05fe9
panhaoyu <panhaoyu.china@outlook.com>
2024-05-11 05:05:09 +0800

    if statement supports new line

--- simple_language_plugin/src/main/gen/org/intellij/sdk/language/SimpleLexer.java
+++ simple_language_plugin/src/main/gen/org/intellij/sdk/language/SimpleLexer.java
@@ -4,9 +4,11 @@
 // Copyright 2000-2022 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.intellij.sdk.language;
 
-import com.intellij.psi.TokenType;
+import com.intellij.formatting.WhiteSpace;import com.intellij.lexer.FlexLexer;
 import com.intellij.psi.tree.IElementType;
+import org.bouncycastle.pqc.jcajce.interfaces.SABERKey;
 import org.intellij.sdk.language.psi.SimpleTypes;
+import com.intellij.psi.TokenType;
 
 
 class SimpleLexer implements FlexLexer {
