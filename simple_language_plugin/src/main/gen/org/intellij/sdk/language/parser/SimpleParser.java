86805aadfcf27b0debfc4cd3684cbba7cfcb4bdf
panhaoyu <panhaoyu.china@outlook.com>
2024-05-10 23:16:05 +0800

    update rules

--- simple_language_plugin/src/main/gen/org/intellij/sdk/language/parser/SimpleParser.java
+++ simple_language_plugin/src/main/gen/org/intellij/sdk/language/parser/SimpleParser.java
@@ -205,15 +205,9 @@ public class SimpleParser implements PsiParser, LightPsiParser {
   }
 
   /* ********************************************************** */
-  // command_scope_inline_fish_statement|function_define|comment_block|command_block
+  // command_scope_inline_fish_statement
   static boolean command_scope(PsiBuilder b, int l) {
-    if (!recursion_guard_(b, l, "command_scope")) return false;
-    boolean r;
-    r = command_scope_inline_fish_statement(b, l + 1);
-    if (!r) r = function_define(b, l + 1);
-    if (!r) r = comment_block(b, l + 1);
-    if (!r) r = command_block(b, l + 1);
-    return r;
+    return command_scope_inline_fish_statement(b, l + 1);
   }
 
   /* ********************************************************** */
