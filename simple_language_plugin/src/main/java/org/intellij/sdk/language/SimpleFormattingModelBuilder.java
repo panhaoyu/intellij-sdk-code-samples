076d1d3452558dcb5145ac62a60e1bd7ab259d81
panhaoyu <panhaoyu.china@outlook.com>
2024-05-10 20:40:29 +0800

    update rules

--- simple_language_plugin/src/main/java/org/intellij/sdk/language/SimpleFormattingModelBuilder.java
+++ simple_language_plugin/src/main/java/org/intellij/sdk/language/SimpleFormattingModelBuilder.java
@@ -2,28 +2,31 @@
 
 package org.intellij.sdk.language;
 
-import com.intellij.formatting.FormattingModelBuilder;
+import com.intellij.formatting.*;
+import com.intellij.psi.codeStyle.CodeStyleSettings;
+import org.intellij.sdk.language.psi.SimpleTypes;
+import org.jetbrains.annotations.NotNull;
 
 final class SimpleFormattingModelBuilder implements FormattingModelBuilder {
 
-//  private static SpacingBuilder createSpaceBuilder(CodeStyleSettings settings) {
-//    return new SpacingBuilder(settings, SimpleLanguage.INSTANCE)
-//        .around(SimpleTypes.OPERATOR)
-//        .spaceIf(settings.getCommonSettings(SimpleLanguage.INSTANCE.getID()).SPACE_AROUND_ASSIGNMENT_OPERATORS)
-//        .before(SimpleTypes.PROPERTY)
-//        .none();
-//  }
-//
-//  @Override
-//  public @NotNull FormattingModel createModel(@NotNull FormattingContext formattingContext) {
-//    final CodeStyleSettings codeStyleSettings = formattingContext.getCodeStyleSettings();
-//    return FormattingModelProvider
-//        .createFormattingModelForPsiFile(formattingContext.getContainingFile(),
-//            new SimpleBlock(formattingContext.getNode(),
-//                Wrap.createWrap(WrapType.NONE, false),
-//                Alignment.createAlignment(),
-//                createSpaceBuilder(codeStyleSettings)),
-//            codeStyleSettings);
-//  }
+  private static SpacingBuilder createSpaceBuilder(CodeStyleSettings settings) {
+    return new SpacingBuilder(settings, SimpleLanguage.INSTANCE)
+        .around(SimpleTypes.OPERATOR)
+        .spaceIf(settings.getCommonSettings(SimpleLanguage.INSTANCE.getID()).SPACE_AROUND_ASSIGNMENT_OPERATORS)
+        .before(SimpleTypes.PROPERTY)
+        .none();
+  }
+
+  @Override
+  public @NotNull FormattingModel createModel(@NotNull FormattingContext formattingContext) {
+    final CodeStyleSettings codeStyleSettings = formattingContext.getCodeStyleSettings();
+    return FormattingModelProvider
+        .createFormattingModelForPsiFile(formattingContext.getContainingFile(),
+            new SimpleBlock(formattingContext.getNode(),
+                Wrap.createWrap(WrapType.NONE, false),
+                Alignment.createAlignment(),
+                createSpaceBuilder(codeStyleSettings)),
+            codeStyleSettings);
+  }
 
 }
