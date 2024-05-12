581ee0f949b9d0c853361b85ffc83a4d10cb3d9c
panhaoyu <panhaoyu.china@outlook.com>
2024-05-13 00:28:08 +0800

    add find usage

--- simple_language_plugin/src/main/java/org/intellij/sdk/language/psi/impl/SimpleNamedElementImpl.java
+++ simple_language_plugin/src/main/java/org/intellij/sdk/language/psi/impl/SimpleNamedElementImpl.java
@@ -4,50 +4,11 @@ package org.intellij.sdk.language.psi.impl;
 
 import com.intellij.extapi.psi.ASTWrapperPsiElement;
 import com.intellij.lang.ASTNode;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.psi.PsiReference;
-import org.intellij.sdk.language.psi.SimpleIdentifierElement;
 import org.intellij.sdk.language.psi.SimpleNamedElement;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.Objects;
 
 public abstract class SimpleNamedElementImpl extends ASTWrapperPsiElement implements SimpleNamedElement {
     public SimpleNamedElementImpl(@NotNull ASTNode node) {
         super(node);
     }
-//
-//    private static final Logger LOG = Logger.getInstance(SimpleNamedElementImpl.class);
-//
-//    @Override
-//    public PsiReference @NotNull [] getReferences() {
-//        PsiReference reference = getReference();
-//        if (Objects.isNull(reference)) {
-//            return new PsiReference[]{};
-//        } else {
-//            return new PsiReference[]{reference};
-//        }
-//    }
-//
-//    @Override
-//    @Nullable
-//    public PsiReference getReference() {
-//        assert this instanceof SimpleIdentifierElement;
-////        if (!(this instanceof SimpleIdentifierElement)) {
-////            return null; // 仅处理IdentifierElement
-////        }
-//        return new SimpleReference(this, this.getTextRange());
-////        Project project = this.getProject();
-////        String currentName = this.getName();
-////
-////        List<SimpleIdentifierElement> declarations = SimpleUtil.findDeclarations(project);
-////
-////        for (SimpleIdentifierElement declaration : declarations) {
-////            String declarationName = declaration.getName();
-////            if (!Objects.equals(declarationName, currentName)) continue;
-////            return new SimpleReference(this, this.getTextRange());
-////        }
-////        return null;
-//    }
 }
