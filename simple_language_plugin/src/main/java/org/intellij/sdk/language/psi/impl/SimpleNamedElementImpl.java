27d661a198db1fd32d4703f97a2d752185a5e32a
panhaoyu <panhaoyu.china@outlook.com>
2024-05-12 16:31:28 +0800

    add reference

--- simple_language_plugin/src/main/java/org/intellij/sdk/language/psi/impl/SimpleNamedElementImpl.java
+++ simple_language_plugin/src/main/java/org/intellij/sdk/language/psi/impl/SimpleNamedElementImpl.java
@@ -6,13 +6,32 @@ import com.intellij.extapi.psi.ASTWrapperPsiElement;
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiReference;
 import org.intellij.sdk.language.SimpleReference;
+import org.intellij.sdk.language.SimpleUtil;
 import org.intellij.sdk.language.psi.SimpleNamedElement;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 public abstract class SimpleNamedElementImpl extends ASTWrapperPsiElement implements SimpleNamedElement {
 
     public SimpleNamedElementImpl(@NotNull ASTNode node) {
         super(node);
+    }
+
+    /**
+     * Checks if this element is the declaration of the identifier.
+     *
+     * @return true if this is the first occurrence of the identifier in the scope.
+     */
+    private boolean isDeclaration() {
+        // Assuming that a declaration in your language is defined as the first occurrence
+        // in the file or a specific structural scope.
+        SimpleNamedElement firstDeclaration = SimpleUtil.findDeclarations(this.getProject()).stream()
+                .filter(e -> Objects.equals(e.getName(), this.getName()) && e.equals(this))
+                .findFirst()
+                .orElse(null);
+
+        return firstDeclaration != null && firstDeclaration.equals(this);
     }
 
     @Override
