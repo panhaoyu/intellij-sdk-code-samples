bee011a715733477194b8829bad7fce8a42ebee1
panhaoyu <panhaoyu.china@outlook.com>
2024-05-12 15:38:49 +0800

    add reference

--- simple_language_plugin/src/main/java/org/intellij/sdk/language/SimpleReferenceContributor.java
+++ simple_language_plugin/src/main/java/org/intellij/sdk/language/SimpleReferenceContributor.java
@@ -6,7 +6,7 @@ import com.intellij.openapi.util.TextRange;
 import com.intellij.patterns.PlatformPatterns;
 import com.intellij.psi.*;
 import com.intellij.util.ProcessingContext;
-import org.intellij.sdk.language.psi.SimpleTypes;
+import org.intellij.sdk.language.psi.SimpleIdentifierElement;
 import org.jetbrains.annotations.NotNull;
 
 import static org.intellij.sdk.language.SimpleAnnotator.SIMPLE_PREFIX_STR;
@@ -18,29 +18,20 @@ final class SimpleReferenceContributor extends PsiReferenceContributor {
     // 重写registerReferenceProviders方法，用于注册引用提供者
     @Override
     public void registerReferenceProviders(@NotNull PsiReferenceRegistrar registrar) {
-        // 注册引用提供者，指定对PsiLiteralExpression类的元素进行处理
-        registrar.registerReferenceProvider(PlatformPatterns.psiElement(PsiLiteralExpression.class),
+        registrar.registerReferenceProvider(
+                PlatformPatterns.psiElement(SimpleIdentifierElement.class),
                 new PsiReferenceProvider() {
-                    // 重写getReferencesByElement方法，用于根据元素生成引用数组
                     @Override
-                    public PsiReference @NotNull [] getReferencesByElement(@NotNull PsiElement element,
-                                                                           @NotNull ProcessingContext context) {
-                        PsiLiteralExpression literalExpression = (PsiLiteralExpression) element;  // 将元素转换为PsiLiteralExpression类型
-                        // 获取字面值表达式的值，如果是字符串，则转换为String类型
-                        String value = literalExpression.getValue() instanceof String ?
-                                (String) literalExpression.getValue() : null;
-                        // 判断值是否以特定的前缀开始
-                        if ((value != null && value.startsWith(SIMPLE_PREFIX_STR + SIMPLE_SEPARATOR_STR))) {
-                            // 计算应该创建引用的文本范围
-                            TextRange property = new TextRange(SIMPLE_PREFIX_STR.length() + SIMPLE_SEPARATOR_STR.length() + 1,
-                                    value.length() + 1);
-                            // 创建并返回SimpleReference数组
-                            return new PsiReference[]{new SimpleReference(element, property)};
+                    public PsiReference @NotNull [] getReferencesByElement(@NotNull PsiElement element, @NotNull ProcessingContext context) {
+                        SimpleIdentifierElement identifier = (SimpleIdentifierElement) element;
+                        String value = identifier.getName();
+                        if (value != null) {
+                            TextRange range = new TextRange(0, value.length());
+                            return new PsiReference[]{new SimpleReference(element, range)};
                         }
-                        // 如果不满足条件，返回空数组
                         return PsiReference.EMPTY_ARRAY;
                     }
-                });
+                }
+        );
     }
-
 }
