553a8eafa489d026b6c29167c12ac5a66dd132ed
panhaoyu <panhaoyu.china@outlook.com>
2024-05-12 23:06:13 +0800

    add find usage

--- simple_language_plugin/src/main/gen/org/intellij/sdk/language/psi/impl/SimpleIdentifierElementImpl.java
+++ simple_language_plugin/src/main/gen/org/intellij/sdk/language/psi/impl/SimpleIdentifierElementImpl.java
@@ -1,49 +1,50 @@
 // This is a generated file. Not intended for manual editing.
 package org.intellij.sdk.language.psi.impl;
 
+import java.util.List;
+import org.jetbrains.annotations.*;
 import com.intellij.lang.ASTNode;
-import com.intellij.navigation.ItemPresentation;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
-import org.intellij.sdk.language.psi.SimpleIdentifierElement;
-import org.intellij.sdk.language.psi.SimpleVisitor;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.psi.util.PsiTreeUtil;
+import static org.intellij.sdk.language.psi.SimpleTypes.*;
+import org.intellij.sdk.language.psi.*;
+import com.intellij.navigation.ItemPresentation;
 
 public class SimpleIdentifierElementImpl extends SimpleNamedElementImpl implements SimpleIdentifierElement {
 
-    public SimpleIdentifierElementImpl(@NotNull ASTNode node) {
-        super(node);
-    }
+  public SimpleIdentifierElementImpl(@NotNull ASTNode node) {
+    super(node);
+  }
 
-    public void accept(@NotNull SimpleVisitor visitor) {
-        visitor.visitIdentifierElement(this);
-    }
+  public void accept(@NotNull SimpleVisitor visitor) {
+    visitor.visitIdentifierElement(this);
+  }
 
-    @Override
-    public void accept(@NotNull PsiElementVisitor visitor) {
-        if (visitor instanceof SimpleVisitor) accept((SimpleVisitor) visitor);
-        else super.accept(visitor);
-    }
+  @Override
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof SimpleVisitor) accept((SimpleVisitor)visitor);
+    else super.accept(visitor);
+  }
 
-    @Override
-    public String getName() {
-        return SimplePsiImplUtil.getName(this);
-    }
+  @Override
+  public String getName() {
+    return SimplePsiImplUtil.getName(this);
+  }
 
-    @Override
-    public PsiElement setName(String newName) {
-        return SimplePsiImplUtil.setName(this, newName);
-    }
+  @Override
+  public PsiElement setName(String newName) {
+    return SimplePsiImplUtil.setName(this, newName);
+  }
 
-    @Override
-    public PsiElement getNameIdentifier() {
-        return SimplePsiImplUtil.getNameIdentifier(this);
-    }
+  @Override
+  public PsiElement getNameIdentifier() {
+    return SimplePsiImplUtil.getNameIdentifier(this);
+  }
 
-
-    @Override
-    public ItemPresentation getPresentation() {
-        return SimplePsiImplUtil.getPresentation(this);
-    }
+  @Override
+  public ItemPresentation getPresentation() {
+    return SimplePsiImplUtil.getPresentation(this);
+  }
 
 }
