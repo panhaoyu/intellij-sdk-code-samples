ba153a3f7355aa1910afd30c58199fbea0eb594e
panhaoyu <panhaoyu.china@outlook.com>
2024-05-13 00:33:44 +0800

    add find usage

--- simple_language_plugin/src/main/java/org/intellij/sdk/language/SimpleFoldingBuilder.java
+++ simple_language_plugin/src/main/java/org/intellij/sdk/language/SimpleFoldingBuilder.java
@@ -26,89 +26,89 @@ import java.util.List;
 
 final class SimpleFoldingBuilder extends FoldingBuilderEx implements DumbAware {
 
-  @Override
-  public FoldingDescriptor @NotNull [] buildFoldRegions(@NotNull PsiElement root,
-                                                        @NotNull Document document,
-                                                        boolean quick) {
-    // Initialize the group of folding regions that will expand/collapse together.
-    FoldingGroup group = FoldingGroup.newGroup(SimpleAnnotator.SIMPLE_PREFIX_STR);
-    // Initialize the list of folding regions
-    List<FoldingDescriptor> descriptors = new ArrayList<>();
+    @Override
+    public FoldingDescriptor @NotNull [] buildFoldRegions(@NotNull PsiElement root,
+                                                          @NotNull Document document,
+                                                          boolean quick) {
+        // Initialize the group of folding regions that will expand/collapse together.
+        FoldingGroup group = FoldingGroup.newGroup(SimpleAnnotator.SIMPLE_PREFIX_STR);
+        // Initialize the list of folding regions
+        List<FoldingDescriptor> descriptors = new ArrayList<>();
 
-    root.accept(new JavaRecursiveElementWalkingVisitor() {
+        root.accept(new JavaRecursiveElementWalkingVisitor() {
 
-      @Override
-      public void visitLiteralExpression(@NotNull PsiLiteralExpression literalExpression) {
-        super.visitLiteralExpression(literalExpression);
+            @Override
+            public void visitLiteralExpression(@NotNull PsiLiteralExpression literalExpression) {
+                super.visitLiteralExpression(literalExpression);
 
-        String value = PsiLiteralUtil.getStringLiteralContent(literalExpression);
-        if (value != null &&
-            value.startsWith(SimpleAnnotator.SIMPLE_PREFIX_STR + SimpleAnnotator.SIMPLE_SEPARATOR_STR)) {
-          Project project = literalExpression.getProject();
-          String key = value.substring(
-              SimpleAnnotator.SIMPLE_PREFIX_STR.length() + SimpleAnnotator.SIMPLE_SEPARATOR_STR.length()
-          );
-          // find SimpleProperty for the given key in the project
-          SimpleProperty simpleProperty = ContainerUtil.getOnlyItem(SimpleUtil.findProperties(project, key));
-          if (simpleProperty != null) {
-            // Add a folding descriptor for the literal expression at this node.
-            descriptors.add(new FoldingDescriptor(literalExpression.getNode(),
-                new TextRange(literalExpression.getTextRange().getStartOffset() + 1,
-                    literalExpression.getTextRange().getEndOffset() - 1),
-                group, Collections.singleton(simpleProperty)));
-          }
-        }
-      }
-    });
+                String value = PsiLiteralUtil.getStringLiteralContent(literalExpression);
+                if (value != null &&
+                        value.startsWith(SimpleAnnotator.SIMPLE_PREFIX_STR + SimpleAnnotator.SIMPLE_SEPARATOR_STR)) {
+                    Project project = literalExpression.getProject();
+                    String key = value.substring(
+                            SimpleAnnotator.SIMPLE_PREFIX_STR.length() + SimpleAnnotator.SIMPLE_SEPARATOR_STR.length()
+                    );
+                    // find SimpleProperty for the given key in the project
+                    SimpleProperty simpleProperty = ContainerUtil.getOnlyItem(SimpleUtil.findProperties(project, key));
+                    if (simpleProperty != null) {
+                        // Add a folding descriptor for the literal expression at this node.
+                        descriptors.add(new FoldingDescriptor(literalExpression.getNode(),
+                                new TextRange(literalExpression.getTextRange().getStartOffset() + 1,
+                                        literalExpression.getTextRange().getEndOffset() - 1),
+                                group, Collections.singleton(simpleProperty)));
+                    }
+                }
+            }
+        });
 
-    return descriptors.toArray(FoldingDescriptor.EMPTY_ARRAY);
-  }
-
-  /**
-   * Gets the Simple Language 'value' string corresponding to the 'key'
-   *
-   * @param node Node corresponding to PsiLiteralExpression containing a string in the format
-   *             SIMPLE_PREFIX_STR + SIMPLE_SEPARATOR_STR + Key, where Key is
-   *             defined by the Simple language file.
-   */
-  @Nullable
-  @Override
-  public String getPlaceholderText(@NotNull ASTNode node) {
-    if (node.getPsi() instanceof PsiLiteralExpression psiLiteralExpression) {
-      String text = PsiLiteralUtil.getStringLiteralContent(psiLiteralExpression);
-      if (text == null) {
-        return null;
-      }
-
-      String key = text.substring(SimpleAnnotator.SIMPLE_PREFIX_STR.length() +
-          SimpleAnnotator.SIMPLE_SEPARATOR_STR.length());
-
-      SimpleProperty simpleProperty = ContainerUtil.getOnlyItem(
-          SimpleUtil.findProperties(psiLiteralExpression.getProject(), key)
-      );
-      if (simpleProperty == null) {
-        return StringUtil.THREE_DOTS;
-      }
-
-      String propertyValue = simpleProperty.getValue();
-      // IMPORTANT: keys can come with no values, so a test for null is needed
-      // IMPORTANT: Convert embedded \n to backslash n, so that the string will look
-      // like it has LF embedded in it and embedded " to escaped "
-      if (propertyValue == null) {
-        return StringUtil.THREE_DOTS;
-      }
-
-      return propertyValue
-          .replaceAll("\n", "\\n")
-          .replaceAll("\"", "\\\\\"");
+        return descriptors.toArray(FoldingDescriptor.EMPTY_ARRAY);
     }
 
-    return null;
-  }
+    /**
+     * Gets the Simple Language 'value' string corresponding to the 'key'
+     *
+     * @param node Node corresponding to PsiLiteralExpression containing a string in the format
+     *             SIMPLE_PREFIX_STR + SIMPLE_SEPARATOR_STR + Key, where Key is
+     *             defined by the Simple language file.
+     */
+    @Nullable
+    @Override
+    public String getPlaceholderText(@NotNull ASTNode node) {
+        if (node.getPsi() instanceof PsiLiteralExpression psiLiteralExpression) {
+            String text = PsiLiteralUtil.getStringLiteralContent(psiLiteralExpression);
+            if (text == null) {
+                return null;
+            }
 
-  @Override
-  public boolean isCollapsedByDefault(@NotNull ASTNode node) {
-    return true;
-  }
+            String key = text.substring(SimpleAnnotator.SIMPLE_PREFIX_STR.length() +
+                    SimpleAnnotator.SIMPLE_SEPARATOR_STR.length());
+
+            SimpleProperty simpleProperty = ContainerUtil.getOnlyItem(
+                    SimpleUtil.findProperties(psiLiteralExpression.getProject(), key)
+            );
+            if (simpleProperty == null) {
+                return StringUtil.THREE_DOTS;
+            }
+
+            String propertyValue = simpleProperty.getName();
+            // IMPORTANT: keys can come with no values, so a test for null is needed
+            // IMPORTANT: Convert embedded \n to backslash n, so that the string will look
+            // like it has LF embedded in it and embedded " to escaped "
+            if (propertyValue == null) {
+                return StringUtil.THREE_DOTS;
+            }
+
+            return propertyValue
+                    .replaceAll("\n", "\\n")
+                    .replaceAll("\"", "\\\\\"");
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean isCollapsedByDefault(@NotNull ASTNode node) {
+        return true;
+    }
 
 }
