39d31c09068045f60ecfc17fcbcd345960764e1b
panhaoyu <panhaoyu.china@outlook.com>
2024-05-14 00:10:48 +0800

    add structure view

--- simple_language_plugin/src/main/java/org/intellij/sdk/language/SimpleStructureAwareNavbar.java
+++ simple_language_plugin/src/main/java/org/intellij/sdk/language/SimpleStructureAwareNavbar.java
@@ -5,41 +5,41 @@ package org.intellij.sdk.language;
 import com.intellij.icons.AllIcons;
 import com.intellij.ide.navigationToolbar.StructureAwareNavBarModelExtension;
 import com.intellij.lang.Language;
+import org.intellij.sdk.language.psi.SimpleBlockDefine;
 import org.intellij.sdk.language.psi.SimpleFile;
-import org.intellij.sdk.language.psi.SimpleProperty;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import javax.swing.Icon;
+import javax.swing.*;
 
 final class SimpleStructureAwareNavbar extends StructureAwareNavBarModelExtension {
 
-  @NotNull
-  @Override
-  protected Language getLanguage() {
-    return SimpleLanguage.INSTANCE;
-  }
-
-  @Override
-  public @Nullable String getPresentableText(Object object) {
-    if (object instanceof SimpleFile) {
-      return ((SimpleFile) object).getName();
-    }
-    if (object instanceof SimpleProperty) {
-      return ((SimpleProperty) object).getName();
+    @NotNull
+    @Override
+    protected Language getLanguage() {
+        return SimpleLanguage.INSTANCE;
     }
 
-    return null;
-  }
+    @Override
+    public @Nullable String getPresentableText(Object object) {
+        if (object instanceof SimpleFile) {
+            return ((SimpleFile) object).getName();
+        }
+        if (object instanceof SimpleBlockDefine blockDefine) {
+            return blockDefine.getText();
+        }
 
-  @Override
-  @Nullable
-  public Icon getIcon(Object object) {
-    if (object instanceof SimpleProperty) {
-      return AllIcons.Nodes.Property;
+        return null;
     }
 
-    return null;
-  }
+    @Override
+    @Nullable
+    public Icon getIcon(Object object) {
+        if (object instanceof SimpleBlockDefine) {
+            return AllIcons.Nodes.Function;
+        }
+
+        return null;
+    }
 
 }
