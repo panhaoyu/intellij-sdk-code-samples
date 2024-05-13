ae5503940f7dcb57dda92cda8749169c85248758
panhaoyu <panhaoyu.china@outlook.com>
2024-05-14 00:04:13 +0800

    add structure view

--- simple_language_plugin/src/main/java/org/intellij/sdk/language/SimpleStructureViewElement.java
+++ simple_language_plugin/src/main/java/org/intellij/sdk/language/SimpleStructureViewElement.java
@@ -9,9 +9,9 @@ import com.intellij.ide.util.treeView.smartTree.TreeElement;
 import com.intellij.navigation.ItemPresentation;
 import com.intellij.psi.NavigatablePsiElement;
 import com.intellij.psi.util.PsiTreeUtil;
+import org.intellij.sdk.language.psi.SimpleBlockDefine;
 import org.intellij.sdk.language.psi.SimpleFile;
-import org.intellij.sdk.language.psi.SimpleProperty;
-import org.intellij.sdk.language.psi.impl.SimplePropertyImpl;
+import org.intellij.sdk.language.psi.impl.SimpleBlockDefineImpl;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -19,57 +19,57 @@ import java.util.List;
 
 public class SimpleStructureViewElement implements StructureViewTreeElement, SortableTreeElement {
 
-  private final NavigatablePsiElement myElement;
+    private final NavigatablePsiElement myElement;
 
-  public SimpleStructureViewElement(NavigatablePsiElement element) {
-    this.myElement = element;
-  }
-
-  @Override
-  public Object getValue() {
-    return myElement;
-  }
-
-  @Override
-  public void navigate(boolean requestFocus) {
-    myElement.navigate(requestFocus);
-  }
-
-  @Override
-  public boolean canNavigate() {
-    return myElement.canNavigate();
-  }
-
-  @Override
-  public boolean canNavigateToSource() {
-    return myElement.canNavigateToSource();
-  }
-
-  @NotNull
-  @Override
-  public String getAlphaSortKey() {
-    String name = myElement.getName();
-    return name != null ? name : "";
-  }
-
-  @NotNull
-  @Override
-  public ItemPresentation getPresentation() {
-    ItemPresentation presentation = myElement.getPresentation();
-    return presentation != null ? presentation : new PresentationData();
-  }
-
-  @Override
-  public TreeElement @NotNull [] getChildren() {
-    if (myElement instanceof SimpleFile) {
-      List<SimpleProperty> properties = PsiTreeUtil.getChildrenOfTypeAsList(myElement, SimpleProperty.class);
-      List<TreeElement> treeElements = new ArrayList<>(properties.size());
-      for (SimpleProperty property : properties) {
-        treeElements.add(new SimpleStructureViewElement((SimplePropertyImpl) property));
-      }
-      return treeElements.toArray(new TreeElement[0]);
+    public SimpleStructureViewElement(NavigatablePsiElement element) {
+        this.myElement = element;
     }
-    return EMPTY_ARRAY;
-  }
+
+    @Override
+    public Object getValue() {
+        return myElement;
+    }
+
+    @Override
+    public void navigate(boolean requestFocus) {
+        myElement.navigate(requestFocus);
+    }
+
+    @Override
+    public boolean canNavigate() {
+        return myElement.canNavigate();
+    }
+
+    @Override
+    public boolean canNavigateToSource() {
+        return myElement.canNavigateToSource();
+    }
+
+    @NotNull
+    @Override
+    public String getAlphaSortKey() {
+        String name = myElement.getName();
+        return name != null ? name : "";
+    }
+
+    @NotNull
+    @Override
+    public ItemPresentation getPresentation() {
+        ItemPresentation presentation = myElement.getPresentation();
+        return presentation != null ? presentation : new PresentationData();
+    }
+
+    @Override
+    public TreeElement @NotNull [] getChildren() {
+        if (myElement instanceof SimpleFile) {
+            List<SimpleBlockDefine> properties = PsiTreeUtil.getChildrenOfTypeAsList(myElement, SimpleBlockDefine.class);
+            List<TreeElement> treeElements = new ArrayList<>(properties.size());
+            for (SimpleBlockDefine property : properties) {
+                treeElements.add(new SimpleStructureViewElement((SimpleBlockDefineImpl) property));
+            }
+            return treeElements.toArray(new TreeElement[0]);
+        }
+        return EMPTY_ARRAY;
+    }
 
 }
