6d3f8285e5dda77653dd0d9683f87748e9124b8c
panhaoyu <panhaoyu.china@outlook.com>
2024-05-14 00:02:14 +0800

    add structure view

--- simple_language_plugin/src/main/java/org/intellij/sdk/language/SimpleStructureViewModel.java
+++ simple_language_plugin/src/main/java/org/intellij/sdk/language/SimpleStructureViewModel.java
@@ -8,36 +8,37 @@ import com.intellij.ide.structureView.StructureViewTreeElement;
 import com.intellij.ide.util.treeView.smartTree.Sorter;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiFile;
+import org.intellij.sdk.language.psi.SimpleBlockDefine;
 import org.intellij.sdk.language.psi.SimpleProperty;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class SimpleStructureViewModel extends StructureViewModelBase implements
-    StructureViewModel.ElementInfoProvider {
+        StructureViewModel.ElementInfoProvider {
 
-  public SimpleStructureViewModel(@Nullable Editor editor, PsiFile psiFile) {
-    super(psiFile, editor, new SimpleStructureViewElement(psiFile));
-  }
+    public SimpleStructureViewModel(@Nullable Editor editor, PsiFile psiFile) {
+        super(psiFile, editor, new SimpleStructureViewElement(psiFile));
+    }
 
-  @NotNull
-  public Sorter @NotNull [] getSorters() {
-    return new Sorter[]{Sorter.ALPHA_SORTER};
-  }
+    @NotNull
+    public Sorter @NotNull [] getSorters() {
+        return new Sorter[]{Sorter.ALPHA_SORTER};
+    }
 
 
-  @Override
-  public boolean isAlwaysShowsPlus(StructureViewTreeElement element) {
-    return false;
-  }
+    @Override
+    public boolean isAlwaysShowsPlus(StructureViewTreeElement element) {
+        return false;
+    }
 
-  @Override
-  public boolean isAlwaysLeaf(StructureViewTreeElement element) {
-    return element.getValue() instanceof SimpleProperty;
-  }
+    @Override
+    public boolean isAlwaysLeaf(StructureViewTreeElement element) {
+        return element.getValue() instanceof SimpleProperty;
+    }
 
-  @Override
-  protected Class<?> @NotNull [] getSuitableClasses() {
-    return new Class[]{SimpleProperty.class};
-  }
+    @Override
+    protected Class<?> @NotNull [] getSuitableClasses() {
+        return new Class[]{SimpleBlockDefine.class};
+    }
 
 }
