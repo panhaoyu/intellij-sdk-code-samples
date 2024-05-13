24de374d215d19cd4a33d6d040f5ad3c0223a071
panhaoyu <panhaoyu.china@outlook.com>
2024-05-13 23:52:45 +0800

    add block

--- simple_language_plugin/src/main/java/org/intellij/sdk/language/SimpleFoldingBuilder.java
+++ simple_language_plugin/src/main/java/org/intellij/sdk/language/SimpleFoldingBuilder.java
@@ -7,16 +7,10 @@ import com.intellij.lang.folding.FoldingBuilderEx;
 import com.intellij.lang.folding.FoldingDescriptor;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.FoldingGroup;
-import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiLiteralExpression;
-import com.intellij.psi.util.PsiLiteralUtil;
-import com.intellij.util.containers.ContainerUtil;
 import org.intellij.sdk.language.psi.SimpleBlockCmd;
 import org.intellij.sdk.language.psi.SimpleBlockDefine;
-import org.intellij.sdk.language.psi.SimpleProperty;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -53,48 +47,9 @@ final class SimpleFoldingBuilder extends FoldingBuilderEx {
     }
 
     // 获取占位文本的方法，通常用于显示折叠时的简略信息
-
-    /**
-     * Gets the Simple Language 'value' string corresponding to the 'key'
-     *
-     * @param node Node corresponding to PsiLiteralExpression containing a string in the format
-     *             SIMPLE_PREFIX_STR + SIMPLE_SEPARATOR_STR + Key, where Key is
-     *             defined by the Simple language file.
-     */
-    @Nullable
     @Override
     public String getPlaceholderText(@NotNull ASTNode node) {
-        if (node.getPsi() instanceof PsiLiteralExpression psiLiteralExpression) {
-            String text = PsiLiteralUtil.getStringLiteralContent(psiLiteralExpression);
-            if (text == null) {
-                return null;
-            }
-
-            String key = text.substring(SimpleAnnotator.SIMPLE_PREFIX_STR.length() +
-                    SimpleAnnotator.SIMPLE_SEPARATOR_STR.length());
-
-            SimpleProperty simpleProperty = ContainerUtil.getOnlyItem(
-                    SimpleUtil.findProperties(psiLiteralExpression.getProject(), key)
-            );
-            if (simpleProperty == null) {
-                return StringUtil.THREE_DOTS;
-            }
-
-            String propertyValue = simpleProperty.getName();
-            // IMPORTANT: keys can come with no values, so a test for null is needed
-            // IMPORTANT: Convert embedded \n to backslash n, so that the string will look
-            // like it has LF embedded in it and embedded " to escaped "
-            if (propertyValue == null) {
-                return StringUtil.THREE_DOTS;
-            }
-
-            // 将换行符和双引号进行转义，以确保文本格式正确
-            return propertyValue
-                    .replaceAll("\n", "\\n")
-                    .replaceAll("\"", "\\\\\"");
-        }
-
-        return null;
+        return node.getText();
     }
 
     // 判断节点在初始化时是否应该默认折叠
