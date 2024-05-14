91780a19b8dd070d5c009879937bc418b557a250
panhaoyu <panhaoyu.china@outlook.com>
2024-05-14 13:54:20 +0800

    remove the usage of java

--- simple_language_plugin/build.gradle.kts
+++ simple_language_plugin/build.gradle.kts
@@ -1,48 +1,48 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 plugins {
-  id("java")
-  id("org.jetbrains.intellij") version "1.17.3"
+    id("java")
+    id("org.jetbrains.intellij") version "1.17.3"
 }
 
 group = "org.intellij.sdk"
 version = "2.0.0"
 
 repositories {
-  mavenCentral()
+    mavenCentral()
 }
 
 // Include the generated files in the source set
 sourceSets {
-  main {
-    java {
-      srcDirs("src/main/gen")
+    main {
+        java {
+            srcDirs("src/main/gen")
+        }
     }
-  }
 }
 
 dependencies {
-  testImplementation("junit:junit:4.13.2")
+    testImplementation("junit:junit:4.13.2")
 }
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 // See https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-  version.set("2023.2.6")
-  plugins.set(listOf("com.intellij.java"))
+    version.set("2023.2.6")
+//  plugins.set(listOf("com.intellij.java"))
 }
 
 tasks {
-  buildSearchableOptions {
-    enabled = false
-  }
+    buildSearchableOptions {
+        enabled = false
+    }
 
-  patchPluginXml {
-    version.set("${project.version}")
-    sinceBuild.set("232")
-    untilBuild.set("241.*")
-  }
+    patchPluginXml {
+        version.set("${project.version}")
+        sinceBuild.set("232")
+        untilBuild.set("241.*")
+    }
 }
