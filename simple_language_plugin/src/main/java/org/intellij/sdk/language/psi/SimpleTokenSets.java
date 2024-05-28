7e51bc88200b52f7d7aa0eabb27b419d1338f6b8
panhaoyu <panhaoyu.china@outlook.com>
2024-05-28 22:05:19 +0800

    add token sets

--- simple_language_plugin/src/main/java/org/intellij/sdk/language/psi/SimpleTokenSets.java
+++ simple_language_plugin/src/main/java/org/intellij/sdk/language/psi/SimpleTokenSets.java
@@ -18,7 +18,10 @@ public interface SimpleTokenSets {
             SimpleTypes.ASSIGNMENT_OPERATOR,
             SimpleTypes.UNARY_OPERATOR,
             SimpleTypes.MINUS_OPERATOR, // 注意，减号并不永远是减号。在command语句里面，减号是可以作为标识符的一部分存在的。
-            SimpleTypes.BINARY_OPERATOR
+            SimpleTypes.BINARY_OPERATOR,
+            SimpleTypes.COMMA_OPERATOR,
+            SimpleTypes.FUNCTION_CALL_OPERATOR,
+            SimpleTypes.DOT_OPERATOR
     );
 
     TokenSet RightBrackets = TokenSet.create(
@@ -32,5 +35,48 @@ public interface SimpleTokenSets {
             SimpleTypes.LEFT_SQUARE_BRACKET
     );
 
+    TokenSet Brackets = TokenSet.create(
+            SimpleTypes.RIGHT_CURLY_BRACKET,
+            SimpleTypes.RIGHT_PARENTHESIS,
+            SimpleTypes.RIGHT_SQUARE_BRACKET,
+            SimpleTypes.LEFT_CURLY_BRACKET,
+            SimpleTypes.LEFT_PARENTHESIS,
+            SimpleTypes.LEFT_SQUARE_BRACKET
+    );
+    TokenSet NewLine = TokenSet.create(
+            SimpleTypes.NEWLINE
+    );
 
+    TokenSet KeyWords = TokenSet.create(
+            SimpleTypes.ARRAY,
+            SimpleTypes.BREAK,
+            SimpleTypes.CASE,
+            SimpleTypes.CASEOF,
+            SimpleTypes.COMMAND,
+            SimpleTypes.COMMENT,
+            SimpleTypes.CONTINUE,
+            SimpleTypes.DEFINE,
+            SimpleTypes.ELSE,
+            SimpleTypes.ELSEIF,
+            SimpleTypes.END,
+            SimpleTypes.ENDCASE,
+            SimpleTypes.ENDCOMMAND,
+            SimpleTypes.ENDIF,
+            SimpleTypes.ENDLOOP,
+            SimpleTypes.ENDSECTION,
+            SimpleTypes.EXIT,
+            SimpleTypes.FISH,
+            SimpleTypes.FOR,
+            SimpleTypes.FOREACH,
+            SimpleTypes.GLOBAL,
+            SimpleTypes.IF,
+            SimpleTypes.LOCAL,
+            SimpleTypes.LOCK,
+            SimpleTypes.LOOP,
+            SimpleTypes.RETURN,
+            SimpleTypes.SECTION,
+            SimpleTypes.STRUCT,
+            SimpleTypes.THEN,
+            SimpleTypes.WHILE
+    );
 }
