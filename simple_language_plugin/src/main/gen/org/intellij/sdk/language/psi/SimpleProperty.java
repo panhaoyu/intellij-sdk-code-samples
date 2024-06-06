4edb593a11b8579a046e8003f3900641f5bc5322
panhaoyu <panhaoyu.china@outlook.com>
2024-06-06 16:54:31 +0800

    删除property的一些用法

--- simple_language_plugin/src/main/gen/org/intellij/sdk/language/psi/SimpleProperty.java
+++ simple_language_plugin/src/main/gen/org/intellij/sdk/language/psi/SimpleProperty.java
@@ -1,10 +1,9 @@
 // This is a generated file. Not intended for manual editing.
 package org.intellij.sdk.language.psi;
 
-import java.util.List;
-import org.jetbrains.annotations.*;
-import com.intellij.psi.PsiElement;
 import com.intellij.navigation.ItemPresentation;
+import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.NotNull;
 
 public interface SimpleProperty extends SimpleNamedElement {
 
