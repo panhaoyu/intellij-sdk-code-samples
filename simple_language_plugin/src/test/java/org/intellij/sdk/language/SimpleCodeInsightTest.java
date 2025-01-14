86533bc137167ecfa45d9ec927ed0c8c8d5534c7
panhaoyu <panhaoyu.china@outlook.com>
2025-01-14 19:43:09 +0800

    使用测试用例

--- simple_language_plugin/src/test/java/org/intellij/sdk/language/SimpleCodeInsightTest.java
+++ simple_language_plugin/src/test/java/org/intellij/sdk/language/SimpleCodeInsightTest.java
@@ -3,38 +3,34 @@
 package org.intellij.sdk.language;
 
 import com.intellij.application.options.CodeStyle;
-import com.intellij.codeInsight.completion.CompletionType;
-import com.intellij.codeInsight.generation.actions.CommentByLineCommentAction;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.psi.codeStyle.CodeStyleManager;
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
-import com.intellij.usageView.UsageInfo;
 
-import java.util.Collection;
 import java.util.List;
 
 public class SimpleCodeInsightTest extends LightJavaCodeInsightFixtureTestCase {
 
-    /**
-     * @return path to test data file directory relative to working directory in the run configuration for this test.
-     */
-    @Override
-    protected String getTestDataPath() {
-        return "src/test/testData";
-    }
-
-    public void testCompletion() {
-        myFixture.configureByFiles("CompleteTestData.java", "DefaultTestData.fis");
-        myFixture.complete(CompletionType.BASIC);
-        List<String> lookupElementStrings = myFixture.getLookupElementStrings();
-        assertNotNull(lookupElementStrings);
-        assertSameElements(lookupElementStrings, "key with spaces", "language", "message", "tab", "website");
-    }
-
-    public void testAnnotator() {
-        myFixture.configureByFiles("AnnotatorTestData.java", "DefaultTestData.fis");
-        myFixture.checkHighlighting(false, false, true, true);
-    }
+//    /**
+//     * @return path to test data file directory relative to working directory in the run configuration for this test.
+//     */
+//    @Override
+//    protected String getTestDataPath() {
+//        return "src/test/testData";
+//    }
+//
+//    public void testCompletion() {
+//        myFixture.configureByFiles("CompleteTestData.java", "DefaultTestData.fis");
+//        myFixture.complete(CompletionType.BASIC);
+//        List<String> lookupElementStrings = myFixture.getLookupElementStrings();
+//        assertNotNull(lookupElementStrings);
+//        assertSameElements(lookupElementStrings, "key with spaces", "language", "message", "tab", "website");
+//    }
+//
+//    public void testAnnotator() {
+//        myFixture.configureByFiles("AnnotatorTestData.java", "DefaultTestData.fis");
+//        myFixture.checkHighlighting(false, false, true, true);
+//    }
 
     public void testFormatter() {
         myFixture.configureByFile("FormatterTestData.fis");
@@ -49,30 +45,30 @@ public class SimpleCodeInsightTest extends LightJavaCodeInsightFixtureTestCase {
         myFixture.checkResultByFile("DefaultTestData.fis");
     }
 
-    public void testRename() {
-        myFixture.configureByFiles("RenameTestData.java", "RenameTestData.fis");
-        myFixture.renameElementAtCaret("websiteUrl");
-        myFixture.checkResultByFile("RenameTestData.fis", "RenameTestDataAfter.fis", false);
-    }
-
-    public void testFolding() {
-        myFixture.configureByFile("DefaultTestData.fis");
-        myFixture.testFolding(getTestDataPath() + "/FoldingTestData.java");
-    }
-
-    public void testFindUsages() {
-        Collection<UsageInfo> usageInfos = myFixture.testFindUsages("FindUsagesTestData.fis", "FindUsagesTestData.java");
-        assertEquals(1, usageInfos.size());
-    }
-
-    public void testCommenter() {
-        myFixture.configureByText(SimpleFileType.INSTANCE, "<caret>website = https://en.wikipedia.org/");
-        CommentByLineCommentAction commentAction = new CommentByLineCommentAction();
-        commentAction.actionPerformedImpl(getProject(), myFixture.getEditor());
-        myFixture.checkResult("#website = https://en.wikipedia.org/");
-        commentAction.actionPerformedImpl(getProject(), myFixture.getEditor());
-        myFixture.checkResult("website = https://en.wikipedia.org/");
-    }
+//    public void testRename() {
+//        myFixture.configureByFiles("RenameTestData.java", "RenameTestData.fis");
+//        myFixture.renameElementAtCaret("websiteUrl");
+//        myFixture.checkResultByFile("RenameTestData.fis", "RenameTestDataAfter.fis", false);
+//    }
+//
+//    public void testFolding() {
+//        myFixture.configureByFile("DefaultTestData.fis");
+//        myFixture.testFolding(getTestDataPath() + "/FoldingTestData.java");
+//    }
+//
+//    public void testFindUsages() {
+//        Collection<UsageInfo> usageInfos = myFixture.testFindUsages("FindUsagesTestData.fis", "FindUsagesTestData.java");
+//        assertEquals(1, usageInfos.size());
+//    }
+//
+//    public void testCommenter() {
+//        myFixture.configureByText(SimpleFileType.INSTANCE, "<caret>website = https://en.wikipedia.org/");
+//        CommentByLineCommentAction commentAction = new CommentByLineCommentAction();
+//        commentAction.actionPerformedImpl(getProject(), myFixture.getEditor());
+//        myFixture.checkResult("#website = https://en.wikipedia.org/");
+//        commentAction.actionPerformedImpl(getProject(), myFixture.getEditor());
+//        myFixture.checkResult("website = https://en.wikipedia.org/");
+//    }
 
 //  public void testReference() {
 //    PsiReference referenceAtCaret =
